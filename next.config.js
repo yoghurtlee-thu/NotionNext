37009c486b581b78817a4c0c403cd3f37f025636
Chlorine <146422759+yoghurtlee-thu@users.noreply.github.com>
2024-04-02 08:24:03 +0800

    fix next.config.js

--- next.config.js
+++ next.config.js
@@ -114,28 +114,4 @@ module.exports = withBundleAnalyzer({
         NODE_ENV_API: process.env.NODE_ENV_API || 'prod',
         THEMES: themes
     }
-    config.resolve.alias['@theme-components'] = path.resolve(
-      __dirname,
-      'themes',
-      THEME
-    )
-    return config
-  },
-  experimental: {
-    scrollRestoration: true
-  },
-  exportPathMap: async function (
-    defaultPathMap,
-    { dev, dir, outDir, distDir, buildId }
-  ) {
-    // export 静态导出时 忽略/pages/sitemap.xml.js ， 否则和getServerSideProps这个动态文件冲突
-    const pages = { ...defaultPathMap }
-    delete pages['/sitemap.xml']
-    return pages
-  },
-  publicRuntimeConfig: {
-    // 这里的配置既可以服务端获取到，也可以在浏览器端获取到
-    NODE_ENV_API: process.env.NODE_ENV_API || 'prod',
-    THEMES: themes
-  }
 })

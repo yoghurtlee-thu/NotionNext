b37fb1abd3949468b7c2e24a4539f48a93aba36e
Chlorine <yoghurtlee123@gmail.com>
2024-04-19 16:01:20 +0800

    fix module problems

--- pages/memos/index.js
+++ pages/memos/index.js
@@ -1,7 +1,7 @@
 import { useRouter } from 'next/router'
 import { getLayoutByTheme } from '@/themes/theme'
 import { siteConfig } from '@/lib/config'
-import { getGlobalData } from '@/lib/db/getNotionData'
+import { getGlobalData } from '@/lib/db/getSiteData'
 import React from 'react'
 import BLOG from '@/blog.config'
 

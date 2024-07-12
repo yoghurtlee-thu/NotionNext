c0796fc447953ad0398eb773bd9648a34c6f9020
Chlorine <chlorine-dev@tuta.io>
2024-07-12 19:21:52 +0800

    Update Footer.js

--- themes/hexo/components/Footer.js
+++ themes/hexo/components/Footer.js
@@ -15,7 +15,7 @@ const Footer = ({ title }) => {
       <i className='fas fa-copyright' /> {`${copyrightDate}`} <span><i className='mx-1 animate-pulse fas fa-heart'/> <a href={siteConfig('LINK')} className='underline font-bold  dark:text-gray-300 '>{siteConfig('AUTHOR')}</a>.<br/>
 
       {siteConfig('BEI_AN') && <><i className='fas fa-shield-alt' /> <a href='https://beian.miit.gov.cn/' className='mr-2'>{siteConfig('BEI_AN')}</a><br/></>}
-      <p><img src="https://s2.loli.net/2024/07/12/xVpF7HBUiRTOYr2.png"> <a href="https://beian.mps.gov.cn/#/query/webSearch?code=11010802044640" rel="noreferrer" target="_blank">京公网安备11010802044640</a> </p>
+      <p><img src="https://s2.loli.net/2024/07/12/xVpF7HBUiRTOYr2.png" /> <a href="https://beian.mps.gov.cn/#/query/webSearch?code=11010802044640" rel="noreferrer" target="_blank">京公网安备11010802044640</a> </p>
 
       <span className='hidden busuanzi_container_site_pv'>
             <i className='fas fa-eye'/><span className='px-1 busuanzi_value_site_pv'> </span>  </span>
